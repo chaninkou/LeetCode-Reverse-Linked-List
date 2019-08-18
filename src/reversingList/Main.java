c104418f6ff4754fed090c02cb1efcaa2cd9c52f
chaninkou <michaelkoulove@yahoo.com>
2019-08-17 23:44:05 -0700

    Not done

---
+++ src/reversingList/Main.java
@@ -0,0 +1,20 @@
+package reversingList;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next= new ListNode(5);
+		
+		ListNode current = head;
+		while(current != null){
+			System.out.print(current.val + " ");
+			
+			current = current.next;
+		}
+		
+	}
+}
---
+++ src/reversingList/ReverseListFunction.java
@@ -0,0 +1,45 @@
+package reversingList;
+
+public class ReverseListFunction {
+	// Iterative solution
+	public ListNode reverseList(ListNode head) {
+		ListNode previousHead = null;
+		ListNode current = head;
+		ListNode next = null;
+		// p c n
+		// null < 1 < 2 3 > 4 > 5
+
+		while (current != null) {
+			next = current.next;
+
+			current.next = previousHead;
+
+			previousHead = current;
+
+			current = next;
+		}
+
+		return previousHead;
+	}
+
+	// Recursion solution
+	// public ListNode reverseList(ListNode head){
+	// if(head == null || head.next == null){
+	// return head;
+	// }
+
+	// ListNode reversedListHead = reverseList(head.next);
+
+	// // 1 -> 2 -> 3 -> 4 <- 5 null
+	// // 1 -> 2 -> 3 <- 4 <- 5 null
+	// head.next.next = head;
+
+	// // null <- 4 <- 5
+	// // null <- 3 <- 4 <- 5
+	// head.next = null;
+
+	// // return 4
+	// // return 3
+	// return reversedListHead;
+	// }
+}
