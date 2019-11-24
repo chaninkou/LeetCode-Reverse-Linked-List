63fa967a9371a858aa8177a999f57dd11b8f4f89
chaninkou <michaelkoulove@yahoo.com>
2019-11-23 23:11:11 -0800

    Great review on reversed linked list, both recursive or iterative.

--- src/reversingList/ReverseLinkedListFunction.java
+++ src/reversingList/ReverseLinkedListFunction.java
@@ -27,28 +27,30 @@ public class ReverseLinkedListFunction {
 	}
 
 	// Recursion solution, O(n) time but O(n) space because call stack
-//	 public ListNode reverseList(ListNode head){
-//		 // When it gets to the end of the node, return that head as newHead
-//		 if(head == null || head.next == null){
-//			 return head;
-//		 }
-//		 
-//		 // Keep reversing the next node
-//		 ListNode newHead = reverseList(head.next);
-//	
-//		 // 1 -> 2 -> 3 -> 4 <- 5 null
-//		 // 1 -> 2 -> 3 <- 4 <- 5 null
-//		 // This will make the next node to point back to current node
-//		 head.next.next = head;
-//	
-//		 // null <- 4 <- 5
-//		 // null <- 3 <- 4 <- 5
-//		 // null as place holder
-//		 head.next = null;
-//
-//		 // return 4
-//		 // return 3
-//		 // Returning the new head
-//		 return newHead;
-//	 }
+	 public ListNode reverseList2(ListNode head){
+		 // When it gets to the end of the node, return that head as newHead
+		 // head == null is in case there is not a linked list at all
+		 if(head == null || head.next == null){
+			 return head;
+		 }
+		 
+		 // Keep reversing the next node
+		 ListNode newHead = reverseList2(head.next);
+	
+		 // 1 -> 2 -> 3 -> 4 <- 5 null
+		 // 1 -> 2 -> 3 <- 4 <- 5 null
+		 // This will make the next node to point back to current node
+		 head.next.next = head;
+	
+		 // null <- 4 <- 5
+		 // null <- 3 <- 4 <- 5
+		 // null as place holder
+		 head.next = null;
+
+		 // return 5
+		 // return 4
+		 // return 3
+		 // Returning the new head
+		 return newHead;
+	 }
 }
